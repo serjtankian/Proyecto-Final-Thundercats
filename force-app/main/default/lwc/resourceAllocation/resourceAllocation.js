dcd44dd93105989b9aebc7ba08bdf4d07ff6eaa6
Dylan <dylansch7@gmail.com>
2022-05-24 18:03:57 -0300

    resource alocation mockup

---
+++ force-app/main/default/lwc/resourceAllocation/resourceAllocation.js
@@ -0,0 +1,17 @@
+import { LightningElement } from 'lwc';
+
+export default class ResourceAllocation extends LightningElement {
+    // Aca podria guardar los datos que me lleguen de la query que hagan en Apex, luego puedo mostrar
+    options = [
+        { label: 'Ross', value: 'option1' },
+        { label: 'Rachel', value: 'option2' },
+    ];
+
+    // Select option1 by default
+    value = ['option1'];
+
+    handleChange(event) {
+        const changeValue = event.detail.value;
+        alert(changeValue);
+    }
+}
