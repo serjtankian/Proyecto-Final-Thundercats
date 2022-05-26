b6885f5ea2f568096beaa64746dae0f6bd4fbde8
gmzleo90 <gmz_leo@hotmail.com>
2022-05-26 12:10:16 -0700

    dev

--- force-app/main/default/lwc/resourceAllocation/resourceAllocation.js
+++ force-app/main/default/lwc/resourceAllocation/resourceAllocation.js
@@ -1,6 +1,10 @@
-import { LightningElement} from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import getAvailableResources from '';
 
 export default class ResourceAllocation extends LightningElement {
+
+    @api recordId; //Obtengo el Id del proyecto que en ese momento esta abierto en salesforce
+
     // Aca podria guardar los datos que me lleguen de la query que hagan en Apex, luego puedo mostrar
     /* options = [
         { label: 'Ross', value: 'option1' },
@@ -9,15 +13,23 @@ export default class ResourceAllocation extends LightningElement {
 
     // Roles (hardcodeados)
     roles = [
-        {name:"CONSULTANTS"},{name:"DEVELOPERS"},{name:"ARCHITECTS"}
+        { name: "CONSULTANTS" }, { name: "DEVELOPERS" }, { name: "ARCHITECTS" }
     ];
 
 
     // Select option1 by default
-
     handleClick(event) {
         console.log(event);
         console.log("ricardo")
         console.log(roles);
+    }
+
+    @wire(getAvailableResources, { projectId: '$recordId'})
+    availableResources;
+
+    renderedCallback() {
+
+        console.log(this.availableResources);
+    
     }
 }
